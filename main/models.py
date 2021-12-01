9f7cc29f5511bba4c06f09b0a47d1bc6b4ab892f
furqanbutt <44622569+furqanbutt@users.noreply.github.com>
2021-12-02 02:25:59 +0500

    Transaction history support

--- main/models.py
+++ main/models.py
@@ -14,3 +14,16 @@ class Product(models.Model):
     dateAdded = models.DateTimeField(default=now())
     location = models.CharField(max_length=255)
     image = models.ImageField(default=None, null=True, upload_to='images/')
+
+    def __str__(self):
+        return "[%s] %s" % (self.productSku, self.productDescription)
+
+
+class Transaction(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantityUsed = models.IntegerField()
+    dateUsed = models.DateTimeField(default=now())
+
+
+    def __str__(self):
+        return "[%s] %s items used at [%s]." % (self.product.productSku, self.quantityUsed, self.dateUsed.__str__())
